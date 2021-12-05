8d67c7a29605e8e3abeeec9105328047b47ad342
Ben <bzhang@willowinc.com>
2021-12-05 11:59:54 +1100

    add associate function test

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,8 +3,11 @@ fn main() {
     let rect2 = Rectangle { width: 10, height: 40 };
     let rect3 = Rectangle { width: 60, height: 45 };
 
+    let square = Rectangle::square(50);
+
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold square? {}", rect1.can_hold(&square));
 }
 
 #[derive(Debug)]
@@ -20,5 +23,9 @@ impl Rectangle {
 
     fn can_hold(&self, another: &Rectangle) -> bool {
         self.width >= another.width && self.height >= another.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
     }
 }
